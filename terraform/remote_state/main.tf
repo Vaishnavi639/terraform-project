b8b1e7dd0be1b2f318bff59492ce4bb3980059cd
Vaishnavi <pangarevaishnavi639@gmail.com>
2024-06-15 16:20:27 +0530

    backend configuration in remote_state

--- terraform/remote_state/main.tf
+++ terraform/remote_state/main.tf
@@ -37,6 +37,17 @@ resource "aws_dynamodb_table" "lock_table" {
 }
 
 
+####################Configuration with Backend[remote storage]#####
+terraform {
+  backend "s3" {
+    bucket         = "vaish-remote-storage-terraform"
+    key            = "ec2-instance/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "remote_storage_locks"
+    encrypt        = true
+  }
+}
+
 
   
 
