467def07dedeccc51ba1a82d6432fc233047a5f5
Vaishnavi <pangarevaishnavi639@gmail.com>
2024-06-15 13:31:57 +0530

    Adding the Backend for storage

---
+++ terraform/remote_state/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "ap-south-1"  
+
+####################Create S3 Bucket################################
+
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "remote-storage-terraform"
+
+  tags = {
+    Name        = "remote-storage-terraform"
+    Environment = "Production"
+    Project     = "RemoteStorage"
+  }
+}
+
+#################Create DynamoDb table#############################
+
+
+
+resource "aws_dynamodb_table" "lock_table" {
+  name           = "remote_storage_locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"  
+  }
+
+  tags = {
+    Environment = "Production"
+    Project     = "RemoteStorage"
+  }
+}
+
+
+
+  
+
