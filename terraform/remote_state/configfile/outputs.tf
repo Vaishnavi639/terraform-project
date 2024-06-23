9e0fcbcfcecbde5759d7af610e203f83f0329195
Vaishnavi <pangarevaishnavi639@gmail.com>
2024-06-23 22:29:47 +0530

    updated resources in outputs.tf

--- terraform/remote_state/configfile/outputs.tf
+++ terraform/remote_state/configfile/outputs.tf
@@ -14,12 +14,12 @@ output "s3_bucket_region" {
 }
 
 output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_lock.name
+  value       = aws_dynamodb_table.lock_table.name
   description = "The ARN of the DynamoDB table"
 }
 
 output "dynamodb_table_arn" {
-  value       = aws_dynamodb_table.terraform_lock.arn
+  value       = aws_dynamodb_table.lock_table.arn
   description = "The ARN of the DynamoDB table"
 }
 
